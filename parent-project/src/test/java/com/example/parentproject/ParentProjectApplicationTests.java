9999d7690350fb5db8665b5c10ceeaaec5af80e0
parksw1 <parksw1@humuson.com>
2024-12-13 15:38:52 +0900

    상속 연습

---
+++ parent-project/src/test/java/com/example/parentproject/ParentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
